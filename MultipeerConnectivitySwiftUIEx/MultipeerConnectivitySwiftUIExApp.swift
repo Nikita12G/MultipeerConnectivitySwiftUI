c4a063af9ec41d18e03be53206cb7a93f7070e09
Nikita12G <gulyi08@mail.ru>
2024-08-19 21:50:13 +0700

    Initial Commit

---
+++ MultipeerConnectivitySwiftUIEx/MultipeerConnectivitySwiftUIExApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipeerConnectivitySwiftUIExApp.swift
+//  MultipeerConnectivitySwiftUIEx
+//
+//  Created by Никита Гуляев on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultipeerConnectivitySwiftUIExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
